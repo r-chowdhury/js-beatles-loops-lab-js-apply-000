da8aad42443dac90e5e4e30c85c060203a1c3906
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-05-18 20:47:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const instruments = [];
 const theBeatlesPlay = (musicians, instruments) => {
   var i;
   for (i = 0; i <= musicians.length-1; i++) { //musicians and instruments will be equilength.
-    console.log(`${musicians[i]} plays ${instruments[i].`);
+    console.log(musicians[i] + " plays " + instruments[i])
   }
 }
