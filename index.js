ae74ec77051219c60ffb9b390fee78f64ddf1b73
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-05-18 20:46:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const musicians = [];
 const instruments = [];
 
 const theBeatlesPlay = (musicians, instruments) => {
-  //const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  //const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   var i;
   for (i = 0; i <= musicians.length-1; i++) { //musicians and instruments will be equilength.
     console.log(musicians[i] + " plays " + instruments[i]);
